45891db656fcc7094edb406eb3a13f9df991fa7c
Gašper Armič <gasper.armic@dhimahi.com>
2019-04-12 18:34:34 +0200

    Initial commit

---
+++ src/main/java/pohodnik/Login.java
@@ -0,0 +1,23 @@
+package pohodnik;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+
+        stage.setTitle("Prijavno okno");
+        stage.setScene(new Scene(root, 400, 350));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(Login.class, args);
+    }
+}
