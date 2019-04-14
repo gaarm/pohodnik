dec679b0bbbdf50deb63652e486051888d6a7b0a
Gašper Armič <gasper.armic@dhimahi.com>
2019-04-14 20:06:36 +0200

    Exchanged listview with tableview

--- src/main/java/pohodnik/UporabnikController.java
+++ src/main/java/pohodnik/UporabnikController.java
@@ -4,28 +4,25 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.InputEvent;
-import javafx.util.Callback;
-
 import java.sql.SQLException;
 import java.util.List;
 
 public class UporabnikController {
-    protected String searchString;
+
+    private String searchString;
 
     @FXML
-    private ListView<Member> listView;
+    private TableView<Member> tableView;
 
     @FXML
     private TextField textSearch;
 
     public UporabnikController() throws SQLException {
         System.out.println("xxx");
-
-
     }
 
     @FXML
@@ -35,46 +32,7 @@ public class UporabnikController {
         List<Member> memberList = dbConnection.getMembers(textSearch.getText().trim());
 
         ObservableList<Member> data = FXCollections.observableList(memberList);
-
-        listView.setItems(data);
-
-        listView.setCellFactory(new Callback<ListView<Member>, ListCell<Member>>(){
-
-            @Override
-            public ListCell<Member> call(ListView<Member> p) {
-
-                ListCell<Member> cell = new ListCell<Member>(){
-
-                    @Override
-                    protected void updateItem(Member t, boolean bln) {
-                        super.updateItem(t, bln);
-                        if (t != null) {
-                            setText(t.getUsername() + " " + t.getEmail());
-                        }
-                    }
-
-                };
-
-                return cell;
-            }
-        });
-
+        tableView.getItems().setAll(data);
     }
 
-    @FXML
-    void inputField(InputEvent event) {
-        searchString = textSearch.getText();
-        loadList(searchString);
-    }
-
-
-    @FXML
-    public void initialize() {
-        loadList(searchString);
-    }
-
-    public void loadList(String searchString) {
-
-
-    }
 }
