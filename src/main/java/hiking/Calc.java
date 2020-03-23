46777df86a83a69ce9531611dcc8a8043ce1c2b6
Gašper Armič <gasper.armic@dhimahi.com>
2020-03-23 19:02:01 +0100

    Added jacooco

--- src/main/java/hiking/Calc.java
+++ src/main/java/hiking/Calc.java
@@ -1,6 +1,9 @@
 package hiking;
 
 public class Calc {
+    private Calc() {
+        throw new IllegalStateException("Calc class");
+    }
     public static int add(int x, int y) {
         return x+y;
     }
--- src/test/java/hiking/CalcTest.java
+++ src/test/java/hiking/CalcTest.java
@@ -2,7 +2,7 @@ package hiking;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
 
